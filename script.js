6c21131ec834fbed307513555f5603c47283bcea
Irsy <irsadafa03@gmail.com>
2026-08-22 07:09:42 +0800

    Update script.js

--- script.js
+++ script.js
@@ -239,7 +239,7 @@ function startNoteTyping() {
             clearInterval(noteTypeTimer);
             noteSignatureEl.style.opacity = '1';
         }
-    }, 35);
+    }, 20);
 }
 
 function prepareOverlay(target) {
